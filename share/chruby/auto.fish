e25ce3f6b9b245e8775e3808fac372f14643f1b0
Jean Mertz <jean@mertz.fm>
2013-07-01 15:57:40 +0200

    Fix cd'ing into directories

--- share/chruby/auto.fish
+++ share/chruby/auto.fish
@@ -1,28 +1,30 @@
 set -e RUBY_VERSION_FILE
 
 function chruby_auto --on-event fish_prompt
-	set -l cwd (pwd)
-	set -l dir (pwd)
-	set -l version_file ''
+  set -l org (pwd)
+  set -l dir (pwd)
+  set -l found ''
 
-	while not test "$dir" = '/'
-		set version_file "$dir/.ruby-version"
+  while not test "$dir" = '/'
+    set version_file "$dir/.ruby-version"
 
-		if test "$version_file" = "$RUBY_VERSION_FILE"; return
-		else if test -f "$version_file"
-			chruby (cat "$version_file"); or return 1
+    if test "$version_file" = "$RUBY_VERSION_FILE"
+      set found 'true'
+      break
+    else if test -f "$version_file"
+      set found 'true'
+      chruby (cat "$version_file")
+      set -gx RUBY_VERSION_FILE "$version_file"
+      break
+    end
 
-			set -gx RUBY_VERSION_FILE "$version_file"
-			return
-		end
+    cd $dir/..
+    set dir (pwd)
+  end
+  cd $org
 
-		cd $dir/..
-		set dir (pwd)
-	end
-	cd $cwd
-
-	if test -n "$RUBY_VERSION_FILE"
-		chruby_reset
-		set -e RUBY_VERSION_FILE
-	end
+  if test -z "$found"; and test -n "$RUBY_VERSION_FILE"
+    chruby_reset
+    set -e RUBY_VERSION_FILE
+  end
 end
