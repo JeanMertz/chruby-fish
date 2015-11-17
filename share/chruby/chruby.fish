3d279ecdf65f9950ebf0dd1dbd67b70a81bed3f6
Jean Mertz <jean@mertz.fm>
2015-11-17 13:07:20 +0100

    set GEM_PATH correctly
    closes #20

--- share/chruby/chruby.fish
+++ share/chruby/chruby.fish
@@ -70,7 +70,7 @@ function chruby_reset
     if test "$ch_gem_path" = '_'
       set -e GEM_PATH
     else
-      set -gx GEM_PATH $ch_gem_path
+      set -gx GEM_PATH (echo $ch_gem_path | tr : '\n')
     end
   end
 
@@ -109,7 +109,7 @@ function chruby_use
 
   if test (id -u) != '0'
     set -gx GEM_HOME $ch_gem_home
-    set -gx GEM_PATH $ch_gem_path
+    set -gx GEM_PATH (echo $ch_gem_path | tr : '\n')
   end
 end
 
