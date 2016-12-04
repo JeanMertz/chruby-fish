01e7d18a367594ae3eabcc2532e776041f650d8a
Jean Mertz <jean@mertz.fm>
2016-12-05 00:45:57 +0100

    Update Homebrew formula for v0.8.1

--- homebrew/chruby-fish.rb
+++ homebrew/chruby-fish.rb
@@ -1,8 +1,8 @@
 class ChrubyFish < Formula
   desc "Thin wrapper around chruby to make it work with the Fish shell"
   homepage "https://github.com/JeanMertz/chruby-fish#readme"
-  url "https://github.com/JeanMertz/chruby-fish/archive/v0.8.0.tar.gz"
-  sha256 "d74fada4c4e22689d08a715a2772e73776975337640bd036fbfc01d90fbf67b7"
+  url "https://github.com/JeanMertz/chruby-fish/archive/v0.8.1.tar.gz"
+  sha256 "1a0fa95d6be5958edca31a80de3594a563de6f7d09213418db895dda6724c271"
   head "https://github.com/JeanMertz/chruby-fish.git"
 
   depends_on "fish" => :recommended
